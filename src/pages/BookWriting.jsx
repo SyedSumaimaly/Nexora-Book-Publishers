1c64b5b2b8f9b5098ed84eb9d9178ea5c9d5b2ea
Syed Sumaim Ali <syedsumaimaly08@gmail.com>
2025-10-21 15:55:13 -0700

    Put ServiceArea & CounterStats in Book Writing Page
    Just put ServiceArea & CounterStats Components in Book Writing Page

--- src/pages/BookWriting.jsx
+++ src/pages/BookWriting.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import MarketingBannerSection from '../component/MarketingBannerSection'
+import CounterStats from '../component/CounterStatsData'
+import ServicesArea from '../component/ServicesArea'
 
 function BookWriting() {
     return (
-        <MarketingBannerSection />
+        <>
+            <MarketingBannerSection />
+            <ServicesArea/>
+            <CounterStats />
+        </>
     )
 }
 
