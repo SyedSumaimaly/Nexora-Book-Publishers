734d2a03637f1a65d1a989275a6da02472441efd
Syed Sumaim Ali <syedsumaimaly08@gmail.com>
2025-10-28 13:54:53 -0700

    Adding remaining sections in Book Marketing Page

--- src/pages/BookMarketing.jsx
+++ src/pages/BookMarketing.jsx
@@ -3,8 +3,10 @@ import BookMarketingFormSection from '../component/BookMarketingFormSection'
 import MarketingPageAboutSection from '../component/MarketingPageAboutSection'
 import MarketingServicesSection from '../component/MarketingServicesSection'
 import CounterStats from '../component/CounterStatsData'
-import CtaSection from '../component/CtaSection'
 import ConsultationCta from '../component/ConsultationCta'
+import SliderSection from '../component/SliderSection'
+import HowWeOperateSection from '../component/HowWeOperateSection'
+import FaqSection from '../component/FaqSection'
 
 function BookMarketing() {
     return (
@@ -13,7 +15,10 @@ function BookMarketing() {
             <MarketingPageAboutSection />
             <MarketingServicesSection />
             <CounterStats />
-            <ConsultationCta /> 
+            <ConsultationCta />
+            <SliderSection />
+            <HowWeOperateSection />
+            <FaqSection />
         </>
     )
 }
