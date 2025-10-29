af5529a1a3df4d8845951069c1b7017c69fd01fd
Syed Sumaim Ali <syedsumaimaly08@gmail.com>
2025-10-29 11:58:39 -0700

    Added Other Components in Book Editing Page

--- src/pages/BookEditing.jsx
+++ src/pages/BookEditing.jsx
@@ -3,14 +3,22 @@ import BookEditingSection from '../component/BookEditingSection'
 import PolishingDiamondsSection from '../component/PolishingDiamondsSection'
 import CounterStats from '../component/CounterStatsData'
 import BookEditingExpertiseSection from '../component/BookEditingExpertiseSection'
+import ConsultationCta from '../component/ConsultationCta'
+import SliderSection from '../component/SliderSection'
+import HowWeOperateSection from '../component/HowWeOperateSection'
+import FaqSection from '../component/FaqSection'
 
 function BookEditing() {
     return (
         <>
             <BookEditingSection />
             <PolishingDiamondsSection />
-            <CounterStats />
             <BookEditingExpertiseSection />
+            <ConsultationCta />
+            <CounterStats />
+            <SliderSection />
+            <HowWeOperateSection />
+            <FaqSection />
         </>
     )
 }
