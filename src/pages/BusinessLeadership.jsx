69eb0f8e988082a052214c748d03f8bbbea039c5
Syed Sumaim Ali <syedsumaimaly08@gmail.com>
2025-11-03 15:34:19 -0800

    Adding Book Elevation Section & Book Editing Option Grid Component In Business Leadership Page

--- src/pages/BusinessLeadership.jsx
+++ src/pages/BusinessLeadership.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import BusinessBookGhostwritingBanner from '../component/BusinessBookGhostwritingBanner'
 import CounterStats from '../component/CounterStatsData'
+import BookElevationSection from '../component/BookElevationSection'
+import BookEditingOptionsGrid from '../component/BookEditingOptionsGrid'
 
 function BusinessLeadership() {
     return (
         <>
             <BusinessBookGhostwritingBanner />
             <CounterStats />
+            <BookElevationSection />
+            <BookEditingOptionsGrid />
         </>
     )
 }
