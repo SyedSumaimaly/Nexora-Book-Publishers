59112357f38ab0ab8ca22f7d945fb3375681f33f
Syed Sumaim Ali <syedsumaimaly08@gmail.com>
2025-11-04 13:20:54 -0800

    Adding Book Cover Info Component

---
+++ src/component/BookCoverInfo.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const BookCoverInfo = () => {
+    return (
+        <section className="lg:py-14 2xl:px-0 py-12 px-6 bg-gray-100">
+            <div className="container mx-auto">
+                <div className="w-full">
+                    <h2 className="luxury-regular text-[#1D1D1F] lg:text-[64px] text-[30px] leading-tight mb-4">
+                        Beauty is on the Outside
+                    </h2>
+                    <p className="manifest-regular text-[#A7A7A7] text-base leading-[28px]">
+                        No matter how much you hear people say, 'Don't judge a book by its cover', people will always judge
+                        a book by its cover. Metaphorically and literally. So you better ensure you bring your A-game.
+                        That's where our cover copywriting experts come in.
+                    </p>
+                    <p className="manifest-regular text-[#A7A7A7] text-base leading-[28px] my-4">
+                        Using their years of experience in the book industry and closely studying the reader audience, they
+                        have discovered a few tips and tricks to use for book cover text creation. One that grabs the
+                        viewer's attention without fail and ensures they open the
+                        book at the very least. And once it's open, your amazing story hiding inside will do the rest!
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default BookCoverInfo;
--- src/pages/CoverCopyPolish.jsx
+++ src/pages/CoverCopyPolish.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import CoverCopyPolishBanner from '../component/CoverCopyPolishBanner'
+import CounterStats from '../component/CounterStatsData'
+import BookCoverInfo from '../component/BookCoverInfo'
 
 function CoverCopyPolish() {
     return (
         <>
             <CoverCopyPolishBanner />
+            <CounterStats />
+            <BookCoverInfo />
         </>
     )
 }
