53777fda2bf6243e44c01cfdac960dd37b6e3171
Syed Sumaim Ali <syedsumaimaly08@gmail.com>
2025-11-05 23:53:39 +0500

    Adding Starter Package Details Component

---
+++ src/component/StarterPackageDetails.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+const StarterPackageDetails = () => {
+    return (
+        <section className="lg:py-24 2xl:px-0 py-12 px-6">
+            <div className="container mx-auto">
+                <div className="w-full">
+                    <h2 className="luxury-regular text-[#1D1D1F] lg:text-[64px] text-[30px] leading-tight mb-4">
+                        Get. Set. Go!
+                    </h2>
+                    <p className="manifest-regular text-[#A7A7A7] text-base leading-[28px]">
+                        Publish your book without all that publishing mumbo jumbo, we have a deal here already. Take our **Starter Package**, and thank yourself for choosing us.
+                    </p>
+
+                    <h2 className="luxury-regular text-[#1D1D1F] lg:text-[30px] text-[25px] leading-tight my-7">
+                        When Do We Come In?
+                    </h2>
+                    <p className="manifest-regular text-[#A7A7A7] text-base leading-[28px] ">
+                        Imagine this: Your book, looking sharp, ready to steal the show. That’s us.
+                    </p>
+                    <p className="manifest-regular text-[#A7A7A7] text-base leading-[28px] ">
+                        Seeing your name on the cover. It's closer than you think. A **real book** or having your **ebook on Amazon** – easy peazy achievable with our Starter Package.
+                    </p>
+
+                    <h2 className="luxury-regular text-[#1D1D1F] lg:text-[30px] text-[25px] leading-tight my-7">
+                        Support Squad
+                    </h2>
+                    <p className="manifest-regular text-[#A7A7A7] text-base leading-[28px] ">
+                        You’re not alone on this journey. We’re here to cheer you on with tips, tricks, and a whole lot of support.
+                    </p>
+                    <p className="manifest-regular text-[#A7A7A7] text-base leading-[28px] ">
+                        We’ll get your book out there, both in **print and digital**. Amazon, Barnes & Noble. You name it, we’ll get you there.
+                    </p>
+                    <p className="manifest-regular text-[#A7A7A7] text-base leading-[28px] ">
+                        Your book will be the talk of the town. We know it for sure. With **worldwide online distribution**, your readers are just a click away. Plus, we’ll help you build a **website** that’s as awesome as your book.
+                    </p>
+                    <p className="manifest-regular text-[#A7A7A7] text-base leading-[28px] ">
+                        Basically, we’ve got your back. You write the book, we handle the rest. Simple as that.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default StarterPackageDetails;
