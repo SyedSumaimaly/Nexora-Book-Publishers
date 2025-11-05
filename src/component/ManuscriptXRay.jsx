13a8f2d1223b63bcba828fc26d3bc7ce972beaa3
Syed Sumaim Ali <syedsumaimaly08@gmail.com>
2025-11-04 16:05:50 -0800

    Adding Manuscript Component

---
+++ src/component/ManuscriptXRay.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ManuscriptXRay = () => {
+    return (
+        <section className="lg:py-18 2xl:px-0 py-12 px-6">
+            <div className="container mx-auto">
+                <div className="w-full">
+                    <h2 className="luxury-regular text-[#1D1D1F] lg:text-[64px] text-[30px] leading-tight mb-4">
+                        Here’s Your Manuscript's X-Ray
+                    </h2>
+                    <p className="manifest-regular text-[#A7A7A7] text-base leading-[28px]">
+                        So you’re not sure if your book is ready for prime time? We can very well pool in and help you. Our **editorial assessment** is your go-to guide. Or you can call it an **X-ray for your writing**. We dig, dig and dig until we uncover its strengths and pinpointing areas for improvement. Think of it as a tune-up for your story. We’ll tell you straight up what you need to do to get your book in tip-top shape. **Clear, actionable advice** to help you reach your publishing goals.
+                    </p>
+                    <p className="manifest-regular text-[#A7A7A7] text-base leading-[28px] my-4">
+                        Whether you need a quick polish or a complete overhaul, we'll point you in the right direction. We'll help you understand where your manuscript stands and recommend the best path forward. we've got you covered.
+                    </p>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default ManuscriptXRay;
--- src/pages/EditorialAssessment.jsx
+++ src/pages/EditorialAssessment.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import EditorialBanner from '../component/EditorialBanner'
+import CounterStats from '../component/CounterStatsData'
+import ManuscriptXRay from '../component/ManuscriptXRay'
 
 function EditorialAssessment() {
     return (
         <>
             <EditorialBanner />
+            <CounterStats />
+            <ManuscriptXRay />
         </>
     )
 }
