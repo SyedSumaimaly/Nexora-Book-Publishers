17cd6da118a6042b1bc8bdfa7d692f81b45645d6
Syed Sumaim Ali <syedsumaimaly08@gmail.com>
2025-11-06 00:09:51 +0500

    Adding Publishing Shortcut CTA Component

--- src/pages/Starter.jsx
+++ src/pages/Starter.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import StarterPackageBanner from '../component/StarterPackageBanner';
 import CounterStats from '../component/CounterStatsData'
+import StarterPackageDetails from '../component/StarterPackageDetails';
+import PublishingShortcutCTA from '../component/PublishingShortcutCTA';
+import FaqSection from '../component/FaqSection'
+
 
 
 function Starter() {
@@ -8,6 +12,9 @@ function Starter() {
         <>
             <StarterPackageBanner />
             <CounterStats />
+            <StarterPackageDetails />
+            <PublishingShortcutCTA />
+            <FaqSection />
         </>
     )
 }
